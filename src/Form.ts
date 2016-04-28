834c18a8b8b04cc7001a923229e9c86dc37f8577
samundrak <samundrak@yahoo.com>
2016-04-28 18:26:23 +0545

    encapsulation added form input  getter

--- src/Form.ts
+++ src/Form.ts
@@ -2,51 +2,84 @@ Array.prototype.insert = function(index, item) {
 	this.splice(index, 0, item);
 }
 class Form {
-	formOptions: any;
-	form: Element;
-	container: any;
-	replaceString: string = "$input";
+	$formOptions: any;
+	$form: Element;
+	$container: any;
+	$replaceString: string = "$input";
+	$submitCallback :any = undefined;
+	$name: string = undefined;
+	$inputFieldsName: string[] = [];
 	setFormOptions(options: any): void {
-		this.formOptions = options;
+		this.$formOptions = options;
 	}
 
-	getRawHTML(){
-		return this.container.innerHTML;
+	getRawHTML = ():string => this.$container.innerHTML;
+	
+	getInputValue(key:string):string{
+		if(this.$inputFieldsName.indexOf(key) === -1) {
+			console.warn("This key("+key+") is not available");
+			return;
+		}
+		return document.forms[this.$name][key].value;
+	}
+	getAllInputDom():string[]{
+		var allInputValues = [];
+		this.$inputFieldsName.forEach((item) => {
+			allInputValues.push(document.forms[this.$name][item]);
+
+		});
+		return allInputValues;
+	}
+	getInputDom(key:string):string{
+		if (this.$inputFieldsName.indexOf(key) === -1) {
+			console.warn("This key(" + key + ") is not available");
+			return;
+		}
+		return document.forms[this.$name][key];
+	}
+	getAllInputValue():any{
+		var allInputValues = {};
+		this.$inputFieldsName.forEach((item)=>{
+			allInputValues[item] = document.forms[this.$name][item].value;
+		});
+		return allInputValues;
 	}
 	getContainer(){
-		return this.container;
+		return this.$container;
 	}
 	create(fields: any): void {
-		if (!this.formOptions) {
-			console.warn("Form options must be set, to set form options you can call setFormOptions method");
+		if (!this.$formOptions) {
+			console.warn("Form options must be set, to set form options you can call set$formOptions method");
 			return;
 		}
 		var form = this.buildForm();
 		this.placeTheForm(form);
 		this.buildChilds();
 	}
-
+	getValue(name:string){
+		console.log()
+	}
 	private placeTheForm(form: any): void {
-		if (!this.formOptions.hasOwnProperty('container')) return;
-		var container = this.formOptions.container;
+		if (!this.$formOptions.hasOwnProperty('container')) return;
+		var container = this.$formOptions.container;
 		if (container.startsWith('.')) {
 			// console.log('This is class');
 			var tagName: NodeListOf<Element> = document
 				.getElementsByClassName(container.substring(1, container.length))
 			if (tagName.length) {
-				this.container = tagName;
+				this.$container = tagName;
 				tagName[0].appendChild(form);
 			}
 		}
 		else if (container.startsWith('#')) {
 			// console.log('This is id');
-			this.container = document.getElementById(container.substring(1, container.length));
-			this.container.appendChild(form);
+			this.$container = document.getElementById(container.substring(1, container.length));
+			this.$container.appendChild(form);
 		} else {
 			// console.log('this is element');
 			var tagName: NodeListOf<Element> = document.getElementsByTagName(container);
 			if (tagName.length) {
-				this.container = tagName;
+				this.$container = tagName;
 				tagName[0].appendChild(form);
 			};
 		}
@@ -59,34 +92,38 @@ class Form {
 		var input = document.createElement(tagName);
 						var template: any;
 		input.setAttribute('type', 'text');
-		if (this.formOptions.fields.hasOwnProperty('common')) {
-			if (this.formOptions.fields.common.attr) {
-				for (var key in this.formOptions.fields.common.attr) {
-					input.setAttribute(key, this.formOptions.fields.common.attr[key]);
+		if (this.$formOptions.fields.hasOwnProperty('common')) {
+			if (this.$formOptions.fields.common.attr) {
+				for (var key in this.$formOptions.fields.common.attr) {
+					input.setAttribute(key, this.$formOptions.fields.common.attr[key]);
+
 				}
 			}
-			template = item.attr.hasOwnProperty('template') ? item.attr.template : this.formOptions.fields.common.hasOwnProperty('template') ? this.formOptions.fields.common.template : this.replaceString;
+			template = item.attr.hasOwnProperty('template') ? item.attr.template : this.$formOptions.fields.common.hasOwnProperty('template') ? this.$formOptions.fields.common.template : this.$replaceString;
 		}
 		for (var key in item.attr) {
+			if (key === 'name') {
+				this.$inputFieldsName.push(item.attr[key]);
+			}
 			input.setAttribute(key, item.attr[key]);
 		}
 		return {input:input,template:template};
 	}
 	private buildChilds():void {
-		if (!this.formOptions.hasOwnProperty('fields')) return;
-		if (!this.formOptions.fields.hasOwnProperty('fields')) return;
+		if (!this.$formOptions.hasOwnProperty('fields')) return;
+		if (!this.$formOptions.fields.hasOwnProperty('fields')) return;
 		var submitButton: string;
 		var childElements: any[] = [];
-		for (var key in this.formOptions.fields.fields) {
+		for (var key in this.$formOptions.fields.fields) {
 			// var field = document.createElement("input");
 			switch (key) {
 				case 'input':
-					this.formOptions.fields.fields[key].forEach((item: any) => {
+					this.$formOptions.fields.fields[key].forEach((item: any) => {
 						if (Array.isArray(item.attr)) return;
 						var tempElement = document.createElement('temp');
 						var field = this.createFields('input', item);
 						tempElement.appendChild(field.input);
-						var changedItem = field.template.replace(this.replaceString, tempElement.innerHTML); 
+						var changedItem = field.template.replace(this.$replaceString, tempElement.innerHTML); 
 							if(item.attr.type == 'submit' || item.attr.type === 'Submit'){
 								submitButton = changedItem;
 							}else{
@@ -95,7 +132,7 @@ class Form {
 					});
 					break;
 				case 'select':
-					 this.formOptions.fields.fields[key].forEach((item:any)=>{
+					 this.$formOptions.fields.fields[key].forEach((item:any)=>{
 						 var tempElement = document.createElement('temp');
 						 var field = this.createFields('select', item);
 						 item.options.forEach( (option:any)=>{
@@ -107,16 +144,19 @@ class Form {
 							 field.input.appendChild(tempOption);
 						 });
 						 tempElement.appendChild(field.input);
-						 var changedItem = field.template.replace(this.replaceString, tempElement.innerHTML); 
+						 var changedItem = field.template.replace(this.$replaceString, tempElement.innerHTML); 
 						childElements.push({ index: item.rank || -1, data : changedItem });
 
 					 });
 					break;
 				case 'checkbox':
 				case 'radio':
-				this.formOptions.fields.fields[key].forEach((item:any)=>{
+				this.$formOptions.fields.fields[key].forEach((item:any)=>{
 					var radios: any[] = [];
 					var masterKey:string = key;
+					if (item.name) {
+						this.$inputFieldsName.push(item.name);
+					}
 					var tempRadioButtonHolder: Element = document.createElement('temp');
 							item.fields.forEach((radio:any)=>{
 								var tempElement = document.createElement('temp');
@@ -131,32 +171,34 @@ class Form {
 								tempRadioButtonHolder.appendChild(field);
 								// radios.push(tempElement.innerHTML);
 							});
-							var template = item.template ? item.template : this.formOptions.fields.common.hasOwnProperty('template') ? this.formOptions.fields.common.template : this.replaceString;
-							var changedItem = template.replace(this.replaceString, tempRadioButtonHolder.innerHTML);
+							var template = item.template ? item.template : this.$formOptions.fields.common.hasOwnProperty('template') ? this.$formOptions.fields.common.template : this.this.$replaceString;
+							var changedItem = template.replace(this.$replaceString, tempRadioButtonHolder.innerHTML);
 							childElements.push({ index: item.rank || -1, data: changedItem });
 				});
 					break;
 			}
 		}
-		var sortedChildElements = [];
+		var sortedChildElements:any = [];
 		childElements.forEach((item:any)=>{
 			var index = item.index < 0 ? childElements.length  : item.index; 
 			sortedChildElements.insert(index,item.data);
 		});
 		sortedChildElements.forEach((item:any)=>{
-			this.form.innerHTML = this.form.innerHTML + item;
+			this.$form.innerHTML = this.$form.innerHTML + item;
 		});
-		this.form.innerHTML = this.form.innerHTML + submitButton;
+		this.$form.innerHTML = this.$form.innerHTML + submitButton;
 	}
 	private buildForm() {
 		var form: any = document.createElement("Form");
-		if (this.formOptions === undefined) return;
-		if (this.formOptions.hasOwnProperty('attribute')) {
-			if (typeof this.formOptions.attribute === 'object') {
-				this.setAttributes(this.formOptions.attribute, form);
+		if (this.$formOptions === undefined) return;
+		if (this.$formOptions.hasOwnProperty('attribute')) {
+			if (typeof this.$formOptions.attribute === 'object') {
+				// if(this.)
+				this.setAttributes(this.$formOptions.attribute, form);
+				this.$name = this.$formOptions.attribute.name || Date.now();
 			}
 		}
-		this.form = form;
+		this.$form = form;
 		return form;
 	}
 
